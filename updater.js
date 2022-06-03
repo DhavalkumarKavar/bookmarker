81aa0f25f22fbfe1e817869077c24fb310178834
Dhaval Kavar <Dhavalk@waves.com>
2022-06-03 15:24:35 +0530

    Impleemnted Updater

---
+++ updater.js
@@ -0,0 +1,44 @@
+const { dialog } = require("electron")
+const { autoUpdater } = require("electron-updater")
+
+autoUpdater.logger = require("electron-log")
+autoUpdater.logger.transports.file.level = "info"
+autoUpdater.autoDownload = false
+
+module.exports = () => {
+
+    autoUpdater.checkForUpdates()
+
+    autoUpdater.on('update-available', () => {
+
+        dialog.showMessageBox({
+            type: 'info',
+            title: 'Aplication Update Available',
+            message: 'A new version of bookmarker is available. Do you want to update now?',
+            buttons: ['Update', 'No']
+        }).then(result => {
+            let buttonIndex = result.response
+
+            if (buttonIndex === 0)
+            {
+                autoUpdater.downloadUpdate()
+            }
+        })       
+    })
+
+    autoUpdater.on('update-downloaded', () => {
+        dialog.showMessageBox({
+            type: 'info',
+            title: 'Update ready',
+            message: 'Install and restart now?',
+            buttons: ['Yes', 'Later']
+        }).then(result => {
+            let buttonIndex = result.response
+
+            if (buttonIndex === 0)
+            {
+                autoUpdater.quitAndInstall(false, true)
+            }
+        })  
+    })
+}
